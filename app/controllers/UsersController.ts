12a5d9a3ee35cedb951e3fa12fdb6959a25b2d3a
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2024-11-21 15:41:20 -0400

    fix: corrigindo path no tsconfig

--- app/controllers/UsersController.ts
+++ app/controllers/UsersController.ts
@@ -1,11 +1,18 @@
-import { Database } from "database";
+import { Database } from "database/index";
 import { Request, Response } from "express";
 
 export default class UsersController {
   static async index(request: Request, response: Response) {
     try {
       const users = await Database("users").select("*");
-      response.send(users);
+
+      const novoUser = await Database.insert({
+        nome: "Alexandre",
+        email: "alexandre@gmail.com",
+        senha: "12345678",
+      }).into("users");
+
+      response.send(novoUser);
     } catch (error: any) {
       response.status(500).send(error.message);
     }
